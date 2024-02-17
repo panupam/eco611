c03fe8675393afb3242d7f9270ff6648e168f650
Pranav K Anupam <pranavanupam@yahoo.com>
2024-02-17 18:24:47 +0530

    modified a bit

--- ce6/q1.py
+++ ce6/q1.py
@@ -1,9 +1,10 @@
 import numpy as np 
+z1=False
 for i in range(100): 
     X=np.random.randint(-9,10,25).reshape(5,5)
     A= X@X.T+ np.diag([1,1,1,1,1])
     c=np.random.randint(-9,10,5)
-    z,z1=False,False
+    z=False
     g= lambda A,c,x: A@x+c
     x=np.random.randint(0,100,5)
     d=-g(A,c,x)
@@ -14,17 +15,19 @@ for i in range(100):
         #print(x)
         if np.linalg.norm(g(A,c,x))<1e-6:
             break
-        if count<4 :
+        if count%5<4 :
             d=-g(A,c,x) + (g(A,c,x).dot(g(A,c,x))/ g(A,c,x_p).dot(g(A,c,x_p))) *d
             count+=1
         else:
             d=-g(A,c,x)
             z=True
-            count=0
+            count+=1
         
     if z:
-        print("No, solution doesn't converge in 5 step for",A,c)
+        print("No, solution doesn't converge in 5 step for",A,c,count)
         z1=True
+        
+
 
 
 
