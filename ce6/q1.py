d1e3831024d2e27ea4ce4b08628dcae8ef1eb089
Pranav K Anupam <pranavanupam@yahoo.com>
2024-02-20 22:38:47 +0530

    added with eigvec

--- ce6/q1.py
+++ ce6/q1.py
@@ -1,24 +1,27 @@
 import numpy as np 
 z1=False
-for i in range(100): 
+for i in range(1000): 
     X=np.random.randint(-9,10,25).reshape(5,5)
     A= X@X.T+ np.diag([1,1,1,1,1])
     c=np.random.randint(-9,10,5)
     z=False
     g= lambda A,c,x: A@x+c
     x=np.random.randint(0,100,5)
+    # e=np.linalg.eig(A)[1]
+    # d=e[:,0]
     d=-g(A,c,x)
     count=0
     while np.linalg.norm(g(A,c,x))>1e-6:
-        x_p=x.copy()
+        x_p=x
         x=x-((g(A,c,x).dot(d))/(d.dot(A.dot(d))))*d
-        #print(x)
+        # count+=1
+        # d=e[:,count%5]
+        # print(x)
         if count%5<4 :
             d=-g(A,c,x) + (g(A,c,x).dot(g(A,c,x))/ g(A,c,x_p).dot(g(A,c,x_p))) *d
             count+=1
         else:
-            d=-g(A,c,x)
-            
+            d=-g(A,c,x)           
             count+=1
         
     if count>5:
@@ -32,9 +35,3 @@ for i in range(100):
     
 if not z1:
     print("Verified that all solution converges in 5 steps")
-
-
-        
-
-    
-    
